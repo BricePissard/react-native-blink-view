c08b66ab74b3927de82e8d25efbf0085189c0d12
Brice Pissard <brice@robby.ai>
2017-12-04 12:11:13 +0100

    Blinking Text and any Component / remove 'TimerMixin' dependency / convert class to Component
    - Convert class into a Component.
    - Remove ’TimerMixin’ dependency.
    - Generalize the blinking to any kind of Component.
    - Add a blinking Text sample.

--- BlinkView.js
+++ BlinkView.js
@@ -7,92 +7,120 @@
 import React, { Component } from "react";
 import PropTypes            from "prop-types";
 import { View, Animated }   from "react-native";
-import createReactClass     from "create-react-class";
-import TimerMixin           from "react-timer-mixin";
 
-export default createReactClass({
-  displayName : 'BlinkView',
-  mixins      : [ TimerMixin ],
-
+export default class BlinkView extends Component
+{
   /**
-   * delay timer handled by TimerMixin
-   * @type {null}
+   * Local timer handled function. Destroyed on unmount
+   * @type {Function}
    */
-  _onDelay : null,
+  _onDelay:any = null;
 
   /**
    * Props validation
-   * @type {Object}
+   * @type {Object} Ex: {
+   *   element  : {any}     Blinking element type, can be 'View', 'Text' or any kind of <Element />, Default is 'View'
+   *   children : {any}     Element displayed in within the <BlinkView>{...}</BlinkView>, it can be a {string} or any kind of <Element />, Default is null
+   *   delay    : {number}  Delay between each blinks in miliseconds, Default is 1500 millisec
+   *   blinking : {boolean} Defines if the element is currently blinkink, Default is true
+   * }
    */
-  propTypes: {
+  static propTypes:Object = {
+    element  : PropTypes.any,
     children : PropTypes.any,
     delay    : PropTypes.number,
-    binking  : PropTypes.bool
-  },
+    blinking : PropTypes.bool
+  }
 
   /**
    * Default props
-   * @return {object} props
+   * @return {object} Default props for this element.
    */
-  getDefaultProps():Object
-  {
-    return {
-      children : null,
-      delay    : 1500,
-      binking  : true
-    }
-  },
+  static defaultProps:Object = {
+    element  : View,
+    children : null,
+    delay    : 1500,
+    blinking : true
+  }
 
   /**
    * Init states
    * @return {object} states
    */
-  getInitialState():Object
+  constructor( props:Object, state:Object ):Object
   {
-    return {
-      delay     : this.props.delay,
-      blinkAnim : new Animated.Value( 0 )
-    };
-  },
+    try
+    {
+      super( props );
+
+      this.state = {
+        delay     : props && props.delay || 1500,
+        blinkAnim : new Animated.Value( 0 )
+      };
+    }
+    catch ( err )
+    {
+      console.warn( err );
+    }
+  }
 
   componentWillMount():void
   {
-    if ( this.props.binking === true )
+    try
     {
-      this.clearInterval( this._onDelay );
-      this._onDelay = this.setInterval( ():void =>
+      if ( this.props.blinking === true )
       {
-        this.state.blinkAnim.stopAnimation();
-        Animated.timing( this.state.blinkAnim, {
-          toValue   : this.state.blinkAnim._value === 0 ? 1 : 0,
-          duration  : this.state.delay-1
-        }).start();
-      }, this.state.delay+1 );
+        clearInterval( this._onDelay );
+        this._onDelay = setInterval( ():void =>
+        {
+          this.state.blinkAnim.stopAnimation();
+          Animated.timing( this.state.blinkAnim, {
+            toValue   : this.state.blinkAnim._value === 0 ? 1 : 0,
+            duration  : this.state.delay-1
+          }).start();
+        }, this.state.delay+1 );
+      }
     }
-  },
+    catch ( err )
+    {
+      console.warn( err );
+    }
+  }
 
   componentWillUnmount():void
   {
-    this.clearInterval( this._onDelay );
-  },
+    try
+    {
+      clearInterval( this._onDelay );
+    }
+    catch ( err )
+    {
+      console.warn( err );
+    }
+  }
 
-  render():View
+  render():any
   {
-    return ( this.props.binking === true ) ? (
-      <Animated.View
-        {...this.props}
-        style = {[ this.props.style, { opacity: this.state.blinkAnim.interpolate({
-          inputRange  : [ 0, 1 ],
-          outputRange : [ 0, 1 ],
-        })} ]}>
-        {this.props.children}
-      </Animated.View>
-    )
-    :
-    <View {...this.props}>
-      {this.props.children}
-    </View>
-    ;
-  },
+    try
+    {
+      const isBlinking:bolean = this.props && this.props.blinking || true;
+      const Element:any = ( ( isBlinking === true ) ? Animated.createAnimatedComponent( this.props && this.props.element || View ) : this.props && this.props.element || View );
 
-});
+      return  (
+        <Element
+          {...this.props}
+          style = {[this.props.style, { opacity: ( isBlinking === true ) ? this.state.blinkAnim : 1 } ]}>
+          {this.props && this.props.children || null}
+        </Element>
+      )
+    }
+    catch ( err )
+    {
+      console.warn( err );
+    }
+    return (
+      this.props && this.props.children || <View />
+    );
+  }
+
+}
--- README.md
+++ README.md
@@ -1,43 +1,51 @@
-# react-native-blink-view [![Join the chat at https://gitter.im/RobbyAssistant](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/RobbyAssistant?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/6e8f99d4499f414b933528a5a7e739b0)](https://www.codacy.com/app/BricePissard/react-native-blink-view?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=BricePissard/react-native-blink-view&amp;utm_campaign=Badge_Grade) [![NPM version](https://badge.fury.io/js/react-native-blink-view.svg)](http://badge.fury.io/js/react-native-blink-view)[![npm downloads](https://img.shields.io/npm/dt/react-native-blink-view.svg?maxAge=2592000)](http://www.npmtrends.com/react-native-blink-view)
+# react-native-blink-view [![Join the chat at https://gitter.im/RobbyAssistant](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/RobbyAssistant?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/6e8f99d4499f414b933528a5a7e739b0)](https://www.codacy.com/app/BricePissard/react-native-blink-view?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=BricePissard/react-native-blink-view&amp;utm_campaign=Badge_Grade) [![NPM version](https://badge.fury.io/js/react-native-blink-view.svg)](http://badge.fury.io/js/react-native-blink-view) [![npm downloads](https://img.shields.io/npm/dt/react-native-blink-view.svg?maxAge=2592000)](http://www.npmtrends.com/react-native-blink-view)
 
 [![NPM](https://nodei.co/npm/react-native-blink-view.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/react-native-blink-view/)
 
-A Simple &lt;View> handler that allows a smooth blinking opacity animation.
+A Simple &lt;View> handler that allows a smooth blinking opacity animation on any animatable Component.
 
 ## Installation
 ```sh
-npm i --save react-native-blink-view
+react-native install react-native-blink-view
 ```
 
 # Props
 | name | desc | type | default
 | --- | --- | --- | --- |
-| `binking` | Is the element binking. | boolean | `true`
+| `blinking` | Is the element blinking. | boolean | `true`
 | `delay` | Delay of each blink in milliseconds.  | number | `1500`
+| `element` | Blinking element type, can be 'View', 'Text' or any kind of <Element /> | Component | `View`
 
 
 ## Usage
 ```javascript
 import BlinkView from 'react-native-blink-view';
 
-// Simple blinking button that blink every 2 sec.
+// 1 - Blinking button that blink every 2 sec.
+// 2 - Blinking word in a text.
 render() {
   return (
-    <BlinkView binking={this.state.isBlinking?true:false} delay={2000}>
-      <Button onPress={e=>{}}>
-        <Text>Now</Text>
-      </Button>
-    </BlinkView>
+    <View>
+
+      <BlinkView blinking={this.state.isBlinking?true:false} delay={2000}>
+        <Button onPress={e=>{}}>
+          <Text>Now</Text>
+        </Button>
+      </BlinkView>
+
+      <Text>
+        <Text>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa</Text>, quae ab illo inventore <BlinkView element={Text}>veritatis</BlinkView><Text> et quasi architecto beatae vitae dicta sunt...</Text>
+      </Text
+
+    </View>
   );
 }
 ```
 
 ### Screenshot
- ![Screenshot of the example](https://github.com/BricePissard/react-native-blink-view/blob/master/assets/img/loop.gif)
+ ![Screenshot of the blinking View sample](https://github.com/BricePissard/react-native-blink-view/blob/master/assets/img/loop.gif)
 
-
-### Dependenies
- [react-timer-mixin](https://github.com/reactjs/react-timer-mixin)
+ ![Screenshot of the example](https://github.com/BricePissard/react-native-blink-view/blob/master/assets/img/text.gif)
 
 ### Development
 PRs highly appreciated
